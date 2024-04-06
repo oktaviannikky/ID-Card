e45e058fc96170107a1fff8be95b2240932abaef
Oktavian Nikky Abprianto <oktaviannikky282@gmail.com>
2024-04-06 10:09:09 +0700

    Update PersonData.vue
    Adding the style into it.

--- src/components/PersonData.vue
+++ src/components/PersonData.vue
@@ -54,6 +54,70 @@ const PersonData = ref ([
 </template>
 
 
-<style>
+<style scoped>
+    .user-container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    gap: 20px;
+    }
 
+    .profile-card {
+    width: 450px;
+    background-color: #fff8e3;
+    border: 1px solid #c6c6c6;
+    border-radius: 8px;
+    padding: 20px;
+    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+    }
+
+    .user-info {
+    display: flex;
+    align-items: center;
+    margin-bottom: 15px;
+    }
+
+    .picture-profile {
+    width: 100px;
+    height: 100px;
+    overflow: hidden;
+    border-radius: 50%;
+    }
+
+    .picture {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    }
+
+    .profile-container {
+    margin-left: 20px;
+    }
+
+    .profile-container p {
+    margin: 5px 0;
+    }
+
+    .hobby {
+    margin-top: 10px;
+    }
+
+    .user-hobbies {
+    font-weight: bold;
+    margin-bottom: 5px;
+    }
+
+    .list-hobbies {
+    display: flex;
+    gap: 5px;
+    }
+
+    .list-hobbies img {
+    width: 50px;
+    height: 50px;
+    object-fit: cover;
+    border-radius: 50%;
+    }
 </style>
